44d491cf115f171b055b48dd65ba78e1214923bc
KarthikNayak024 <karunayak63@gmail.com>
2020-06-15 11:20:56 +0530

    Added delay to create animations

--- N-queens.js
+++ N-queens.js
@@ -6,74 +6,79 @@ const queen = '<i class="fas fa-chess-queen" style="color:blue"></i>';
 
 let n;
 
-function isValid(r, col, n) {
+async function isValid(r, col, n) {
   // Checking the queen in the same row
   for (let i = 0; i < r; ++i) {
-    let x = 0;
-    while (x != 1000000) {
-      ++x;
-    }
     const row = document.getElementById(`Row${i}`);
-    const value = row.getElementsByTagName("td")[col].innerHTML;
-    if (value == queen) return false;
+    const column = row.getElementsByTagName("td")[col];
+    column.style.backgroundColor = "yellow";
+    const value = column.innerHTML;
+    await new Promise((done) => setTimeout(() => done(), 500));
+
+    if (value == queen) {
+      column.style.backgroundColor = "red";
+      return false;
+    }
   }
 
   //Checking the upper diagonal
   for (let i = r - 1, j = col - 1; i >= 0 && j >= 0; --i, --j) {
-    let x = 0;
-    while (x != 1000000) {
-      ++x;
-    }
     const row = document.getElementById(`Row${i}`);
-    const value = row.getElementsByTagName("td")[j].innerHTML;
-    if (value == queen) return false;
+    const column = row.getElementsByTagName("td")[j];
+    column.style.backgroundColor = "yellow";
+    const value = column.innerHTML;
+    await new Promise((done) => setTimeout(() => done(), 500));
+
+    if (value == queen) {
+      column.style.backgroundColor = "red";
+      return false;
+    }
   }
 
+  // await delay(500);
   for (let i = r - 1, j = col + 1; i >= 0 && j < n; --i, ++j) {
-    let x = 0;
-    while (x != 1000000) {
-      ++x;
-    }
     const row = document.getElementById(`Row${i}`);
-    const value = row.getElementsByTagName("td")[j].innerHTML;
-    if (value == queen) return false;
-  }
+    const column = row.getElementsByTagName("td")[j];
+    column.style.backgroundColor = "yellow";
+    const value = column.innerHTML;
 
+    await new Promise((done) => setTimeout(() => done(), 500));
+
+    if (value == queen) {
+      column.style.backgroundColor = "red";
+      return false;
+    }
+  }
   return true;
 }
 
-function solveQueen(r, n) {
+async function solveQueen(r, n) {
   if (r == n) return true;
 
   for (let i = 0; i < n; ++i) {
-    let x = 0;
-    while (x != 1000000) {
-      ++x;
-    }
-    if (isValid(r, i, n)) {
+    if (await isValid(r, i, n)) {
       const row = document.getElementById(`Row${r}`);
       row.getElementsByTagName("td")[i].innerHTML = queen;
 
-      if (solveQueen(r + 1, n)) return true;
-
+      if (await solveQueen(r + 1, n)) return true;
       row.getElementsByTagName("td")[i].innerHTML = "-";
     }
   }
   return false;
 }
-function nQueen() {
+
+async function nQueen() {
   solveQueen(0, n);
   // if (solveQueen(0, n) == false) alert(`Cannot place all the queens`);
 }
 
 function getValue() {
-  // console.log(textbox.value);
-  // if (textbox.value == 0) alert("Cannot place a queen");
-
   n = textbox.value;
 
-  board.removeChild(board.firstChild); // Removing the first child from the DOM
-
+  // board.removeChild(board.firstChild); // Removing the first child from the DOM
+  while (board.childElementCount != 0) {
+    board.removeChild(board.firstChild);
+  }
   //Adding the same child to the DOM
   if (board.childElementCount == 0) {
     table = document.createElement("TABLE");
@@ -89,9 +94,8 @@ function getValue() {
       col.innerHTML = "-";
     }
   }
-  // console.log(table);
-  // const row1 = document.getElementById("Row0");
-  // const col1 = row1.getElementsByTagName("td");
-  // col1[0].innerHTML = queen; //setting the value of board[0][0] as queen
-  nQueen();
+
+  setTimeout(() => {
+    nQueen();
+  }, 500);
 }
